6460d7c626d2ec24953b9940a96d95f3ae6a89d5
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-07-06 12:05:01 +0200

    remove user profile plugin for replacement

--- plugins/UserProfiles/views/admin/javascripts/userprofiles.js
+++
@@ -1,9 +0,0 @@
-    Omeka.ElementSets.toggleElement = function (button) {
-        if(jQuery(button).nextAll('input.delete').val() != 1) {
-            jQuery(button).nextAll('input.delete').val(1);
-            
-        } else {
-            jQuery(button).nextAll('input.delete').val(0);
-        }
-    };
-
