74915382214161f755adca6ff86ca8ead94ade73
Joris <joris.lambrechts@kuleuven.be>
2017-07-13 09:20:24 +0200

    adjust views new plugins

--- themes/aezel/guest-user/user/confirm.php
+++ themes/aezel/guest-user/user/confirm.php
@@ -2,10 +2,16 @@
 $head = array('title' => __('Confirmation Error'));
 echo head($head);
 ?>
-<h1><?php echo $head['title']?></h1>
-
-<div id='primary'>
-<?php echo flash(); ?>
+<div class="container">
+    <!-- Content -->
+    <div class="content-wrapper bs-docs-section ">
+        <div class="row">
+            <div class="col-xs-12 offset-md-3 col-md-6 content">
+                <h1><?php echo $head['title']?></h1>
+                <?php echo flash(); ?>
+            <div class="col-md-3"><?php //echo simple_nav();?></div>
+        </div>
+    </div>
 </div>
 
 <?php echo foot(); ?>
--- themes/aezel/users/login.php
+++ themes/aezel/users/login.php
@@ -2,15 +2,15 @@
 queue_js_file('login');
 $pageTitle = __('Log In');
 echo head(array('bodyclass' => 'login', 'title' => $pageTitle), $header);
-$namespace = new Zend_Session_Namespace;
-$namespace->redirect = "/vrijwilligers";
+//$namespace = new Zend_Session_Namespace;
+//$namespace->redirect = "/vrijwilligers";
 ?>
 <div class="container">
     <!-- Content -->
     <div class="content-wrapper bs-docs-section ">
         <div class="row">
             <div class="col-xs-12 offset-md-3 col-md-6 content">
-                <h1>Aanmelden als vrijwilliger</h1>
+                <h1>Aanmelden</h1>
                 <p id="login-links">
                 <span id="backtosite"><a href="<?php echo url("/guest-user/user/register"); ?>">Nog niet geregistreerd?</a></span>  |
                 <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span>
@@ -21,7 +21,7 @@ $namespace->redirect = "/vrijwilligers";
                 <?php echo $this->form->setAction($this->url('users/login')); ?>
               </div>
             <div class="col-md-3"><?php //echo simple_nav();?></div>
-            </div>
-            </div>
-            </div>
+        </div>
+    </div>
+</div>
 <?php echo foot(array(), $footer); ?>
