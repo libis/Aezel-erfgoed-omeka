382e6196145c1cb343cf0b945c1f70552c7a156c
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-07-13 12:06:52 +0200

    remove contriution plugin

--- plugins/Contribution/controllers/ItemsController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Contribution_ItemsController extends Omeka_Controller_AbstractActionController
-{
-    public function init()
-    {
-        $this->_helper->db->setDefaultModelName('ContributionContributedItem');
-    }
-}
--- plugins/Contribution/views/admin/javascripts/contribution.js
+++
@@ -1,9 +0,0 @@
-function setUpSettingsWysiwyg() {
-    jQuery(window).load(function() {
-        Omeka.wysiwyg({
-           mode: "specific_textareas",
-           editor_selector: "html-editor",
-           forced_root_block: ""
-        });
-    });
-}
