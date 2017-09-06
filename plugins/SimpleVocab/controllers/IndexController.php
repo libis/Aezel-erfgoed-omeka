6c2ea5b30899ba7afdde59d93bc66a1586a4a064
Joris <joris.lambrechts@kuleuven.be>
2017-09-06 11:30:53 +0200

    fix #37

--- plugins/SimpleVocab/controllers/IndexController.php
+++ plugins/SimpleVocab/controllers/IndexController.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Simple Vocab
- * 
+ *
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
 
 /**
  * The Simple Vocab controller.
- * 
+ *
  * @package Omeka\Plugins\SimpleVocab
  */
 class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionController
@@ -20,11 +20,11 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
     {
         // Restrict actions to AJAX requests.
         $this->_helper->getHelper('AjaxContext')
-                      ->addActionContexts(array('element-texts' => 'html', 
+                      ->addActionContexts(array('element-texts' => 'html',
                                                 'element-terms' => 'html'))
                       ->initContext();
     }
-    
+
     /**
      * Render the edit element terms form.
      */
@@ -33,7 +33,7 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
         $this->view->options_for_select = $this->_getOptionsForSelect();
         $this->view->terms = $this->_helper->db->getTable('SimpleVocabTerm')->findAll();
     }
-    
+
     /**
      * Process the element terms form and redirect to the index action.
      */
@@ -41,14 +41,14 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
     {
         $elementId = $this->getRequest()->getParam('element_id');
         $terms = $this->getRequest()->getParam('terms');
-        
+
         // Don't process the empty element select option.
         if ('' == $elementId) {
             $this->_helper->redirector('index');
         }
-        
+
         $simpleVocabTerm = $this->_helper->db->getTable('SimpleVocabTerm')->findByElementId($elementId);
-        
+
         // Handle an existing term record.
         if ($simpleVocabTerm) {
              // Delete term record if there are no terms.
@@ -59,7 +59,7 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
              }
              $simpleVocabTerm->terms = $this->_sanitizeTerms($terms);
              $this->_helper->flashMessenger(__('Successfully edited the element\'s vocabulary terms.'), 'success');
-        
+
         // Handle a new term record.
         } else {
             // Do not save a new term record without terms.
@@ -74,10 +74,10 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
         $simpleVocabTerm->save();
         $this->_helper->redirector('index');
     }
-    
+
     /**
      * Render the edit terms.
-     * 
+     *
      * Available only via an AJAX request.
      */
     public function elementTermsAction()
@@ -91,17 +91,17 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
         }
         $this->view->terms = $terms;
     }
-    
+
     /**
      * Render the element texts.
-     * 
+     *
      * Available only via an AJAX request.
      */
     public function elementTextsAction()
     {
         $elementId = $this->getRequest()->getParam('element_id');
         $table = $this->_helper->db->getTable('SimpleVocabTerm');
-        
+
         // Get the element's vocab terms, if any.
         $simpleVocabTerm = $table->findByElementId($elementId);
         if ($simpleVocabTerm) {
@@ -109,11 +109,11 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
         } else {
             $terms = array();
         }
-        
-        $warningMessages = array('notInVocab' => __('Not in vocabulary.'), 
-                                 'longText' => __('Long text.'), 
+
+        $warningMessages = array('notInVocab' => __('Not in vocabulary.'),
+                                 'longText' => __('Long text.'),
                                  'containsNewlines' => __('Contains newlines.'));
-        
+
         // Get the element's element texts, if any.
         $elementTexts = array();
         foreach ($table->findElementTexts($elementId) as $elementText) {
@@ -127,24 +127,24 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
             if (strstr($elementText->text, "\n")) {
                 $warnings[] = $warningMessages['containsNewlines'];
             }
-            
-            $elementTexts[] = array('warnings' => $warnings, 
-                                    'count' => $elementText->count, 
+
+            $elementTexts[] = array('warnings' => $warnings,
+                                    'count' => $elementText->count,
                                     'text' => $elementText->text);
         }
-        
+
         $this->view->element_texts = $elementTexts;
     }
-    
+
     /**
      * Get the form select options.
-     * 
+     *
      * @return array
      */
     private function _getOptionsForSelect()
     {
         $elements = $this->_helper->db->getTable('SimpleVocabTerm')->findElementsForSelect();
-        $options = array('' => __('Select Below'));
+        $options = array('' => __('Selecteer uit lijst'));
         foreach ($elements as $element) {
             if ($element['item_type_name']) {
                 $optGroup = __('Item Type') . ': ' . __($element['item_type_name']);
@@ -159,10 +159,10 @@ class SimpleVocab_IndexController extends Omeka_Controller_AbstractActionControl
         }
         return $options;
     }
-    
+
     /**
      * Sanitize the terms for insertion into the database.
-     * 
+     *
      * @param string $terms
      * @return string
      */
